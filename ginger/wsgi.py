8ab6458450cbed1b00cbf2066b42f476e8916ebe
Malcolm Mathew <malcolmmmathew@gmail.com>
2015-10-21 04:20:35 -0400

    fixed db issues

--- ginger/wsgi.py
+++ ginger/wsgi.py
@@ -7,13 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-"""import os
+import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ginger.settings")
 
-application = get_wsgi_application()"""
+application = get_wsgi_application()
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
